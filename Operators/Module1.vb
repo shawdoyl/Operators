7e38683d643b3f17d63b122c117e686f598e7627
Doyle Shaw <shawdoyl@isu.edu>
2020-08-20 08:25:25 -0600

    First Commit

---
+++ Operators/Module1.vb
@@ -0,0 +1,27 @@
+﻿'Doyle Shaw
+'RCET0265
+'Fall 2020
+'Operators
+'https://github.com/shawdoyl/Operators.git
+
+
+Module Module1
+
+    Sub Main()
+
+        Console.WriteLine(2 + 2)
+        Console.ReadLine()
+        Console.WriteLine(7 - 1)
+        Console.ReadLine()
+        Console.WriteLine(5 * 5)
+        Console.ReadLine()
+        Console.WriteLine(7 / 3)
+        Console.ReadLine()
+        Console.WriteLine(7 \ 3)
+        Console.ReadLine()
+        Console.WriteLine(47.9 Mod 9.35)
+        Console.ReadLine()
+
+    End Sub
+
+End Module
