9719c8f30a32ecd107ea1ca341359622caec6fdb
Doyle Shaw <shawdoyl@isu.edu>
2020-08-20 08:45:25 -0600

    Add comparitives

--- Operators/Module1.vb
+++ Operators/Module1.vb
@@ -9,18 +9,43 @@ Module Module1
 
     Sub Main()
 
-        Console.WriteLine(2 + 2)
+
+        'addition
+        'Console.WriteLine(2 + 2)
+        'Console.ReadLine()
+        'Console.WriteLine(7 - 1)
+        'Console.ReadLine()
+        'Console.WriteLine(5 * 5)
+        'Console.ReadLine()
+        'Console.WriteLine(7 / 3)
+        'Console.ReadLine()
+        'Console.WriteLine(7 \ 3)
+        'Console.ReadLine()
+        'Console.WriteLine(47.9 Mod 9.35)
+        'Console.ReadLine()
+
+
+        'concatenate stuff
+        'use with caution
+        'Console.WriteLine("2" + "2")
+        'Console.ReadLine()
+        ''happy times
+        'Console.WriteLine("2" & "2")
+        'Console.ReadLine()
+        ''example
+        'Console.WriteLine("Hello" & "," & " " & "World!")
+        'Console.ReadLine()
+
+        'compare stuff
+        Console.WriteLine(1 > 3)
         Console.ReadLine()
-        Console.WriteLine(7 - 1)
+        Console.WriteLine(1 < 3)
         Console.ReadLine()
-        Console.WriteLine(5 * 5)
+        Console.WriteLine(1 = 3)
         Console.ReadLine()
-        Console.WriteLine(7 / 3)
+        Console.WriteLine(1 <> 3)
         Console.ReadLine()
-        Console.WriteLine(7 \ 3)
-        Console.ReadLine()
-        Console.WriteLine(47.9 Mod 9.35)
-        Console.ReadLine()
+
 
     End Sub
 
